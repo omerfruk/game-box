33afffad29165dacc0cc1b1f67ab903e5c10ebdb
omerfruk <omer.fruk3547@gmail.com>
2021-03-07 14:52:03 +0300

    goinit sayfası eklendi

---
+++ database/init.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"fmt"
+	"github.com/omerfruk/game-box/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+const (
+	HOST     = "localhost"
+	DATABASE = "blog"
+	USER     = "postgres"
+	PASSWORD = "123"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB  {
+	return db
+}
+//db baglantısını gerceklestirecegimiz alan
+func Connect()  {
+	vt := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", HOST, USER, PASSWORD, DATABASE)
+	var err error
+	db, err = gorm.Open(postgres.Open(vt), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{SingularTable: true},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxOpenConns(10)
+}
+//2 methodu birden cagirma metodu
+func ConnectAndMigrate() {
+	Connect()
+	Migrate()
+}
+//db ile models verilerini haberdar edecek method
+func Migrate()  {
+	db.AutoMigrate(models.User{})
+	db.AutoMigrate(models.Account{})
+	db.AutoMigrate(models.Developer{})
+	db.AutoMigrate(models.Score{})
+}
