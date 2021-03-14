1540df301115bdca810efebf7d30a88f9ee125ad
omerfruk <omer.fruk3547@gmail.com>
2021-03-14 17:32:35 +0300

    developers get all methodu eklendi

--- handlers/index_handler.go
+++ handlers/index_handler.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/game-box/service"
 )
 
 func IndexGet(c *fiber.Ctx) error {
+	tempDevelopers := service.GetAllDevelopers()
 
-	return c.Render("index", true)
+	return c.Render("index", tempDevelopers)
 }
