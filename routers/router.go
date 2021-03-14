509c8d42780ba5b55c6038ae1d956259f09e2319
omerfruk <omer.fruk3547@gmail.com>
2021-03-14 14:06:32 +0300

    signup get posy yonlendirmeleri yapıldı

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,12 @@
 package routers
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/game-box/handlers"
+)
 
-func Router()  {
-
+func Router(app *fiber.App) {
+	//signup post ve get metodlari
+	app.Get("/signup", handlers.SignupGet)
+	app.Post("signup", handlers.SignupPost)
 }
