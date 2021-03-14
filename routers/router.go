a27ae161a187752dc57e8d579214456375a1e9d5
omerfruk <omer.fruk3547@gmail.com>
2021-03-14 15:49:24 +0300

    idex sayfası get methodu tanımlandı

--- routers/router.go
+++ routers/router.go
@@ -6,7 +6,8 @@ import (
 )
 
 func Router(app *fiber.App) {
+	app.Get("/", handlers.IndexGet)
 	//signup post ve get metodlari
 	app.Get("/signup", handlers.SignupGet)
-	app.Post("signup", handlers.SignupPost)
+	app.Post("/signup", handlers.SignupPost)
 }
