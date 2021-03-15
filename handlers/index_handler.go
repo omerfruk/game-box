93cc397ca62891d7003c02f04a0b3d6cce25b70b
omerfruk <omer.fruk3547@gmail.com>
2021-03-15 14:20:47 +0300

    session için methodlar yazildi

--- handlers/index_handler.go
+++ handlers/index_handler.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/game-box/models"
 	"github.com/omerfruk/game-box/service"
 )
 
 func IndexGet(c *fiber.Ctx) error {
 	tempDevelopers := service.GetAllDevelopers()
-
-	return c.Render("index", tempDevelopers)
+	sess, err := Store.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	a := models.Islogin{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("index", a)
 }
