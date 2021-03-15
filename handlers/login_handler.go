fe61bf644eabb29e1666b9ec75f7d2da7c7d0f00
omerfruk <omer.fruk3547@gmail.com>
2021-03-15 10:44:27 +0300

    session islemleri yapılıyor

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -3,32 +3,47 @@ package handlers
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/fiber/v2/utils"
 	"github.com/omerfruk/game-box/database"
 	"github.com/omerfruk/game-box/models"
+	"time"
 )
 
 func LoginGet(c *fiber.Ctx) error {
 
 	return c.Render("login", true)
 }
+
+var store = session.New(session.Config{
+	Expiration:   5 * time.Minute,
+	CookieName:   "session_id",
+	KeyGenerator: utils.UUID,
+})
+
 func LoginPost(c *fiber.Ctx) error {
+	var deger models.User
+
 	var temp models.Account
+
 	err := c.BodyParser(&temp)
 	if err != nil {
 		fmt.Println(err)
 	}
-	loginKontrol(temp)
-	// session olusturulacak
-	//giris kontreolu yapılacak
-	//session yoketme olusturlacak
-	return c.Render("", temp)
-}
-
-func loginKontrol(model models.Account) {
-	var temp models.User
-	err := database.DB().Where("mail = ? and password = ?", model.Mail, model.Password).Find(&temp).Error
+	err = database.DB().Where("mail = ? and password = ?", temp.Mail, temp.Password).First(&deger).Error
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(temp)
+	sess, err := store.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer sess.Save()
+	sess.Set("deger", deger.Fullname)
+	sess.Get("deger")
+
+	// session olusturulacak
+	//giris kontreolu yapılacak
+	//session yoketme olusturlacak
+	return c.Redirect("/")
 }
