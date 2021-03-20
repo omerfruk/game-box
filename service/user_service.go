0612601acc64aa374157eb7d36091192a441d544
omerfruk <omer.fruk3547@gmail.com>
2021-03-20 17:16:59 +0300

    edit uster ekeldnii

--- service/user_service.go
+++ service/user_service.go
@@ -8,9 +8,9 @@ import (
 	"gorm.io/gorm"
 )
 
-func CreateUser(user models.User)  error{
+func CreateUser(user models.User) error {
 	password := Sha256String(user.Password)
-	user.Password=password
+	user.Password = password
 	err := database.DB().Where("mail = ? ", user.Mail).First(&user).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		err = database.DB().Create(&user).Error
@@ -21,6 +21,18 @@ func CreateUser(user models.User)  error{
 	}
 	return err
 }
+func EditUser(user models.User) {
+	database.DB().Where("id = ?", user.ID).Save(&user)
+}
+
+//kullanici score ekleme alani
 func AddScore() {
 
+}
+
+//model ile kullaniciyi silme
+func DeleteUser(user models.User) {
+	if err := database.DB().Delete(&user); err != nil {
+		fmt.Println(err)
+	}
 }
