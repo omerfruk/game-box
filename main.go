1ac6110c32148818e0062ab978e7f09d79579456
omerfruk <omer.fruk3547@gmail.com>
2021-03-28 14:20:16 +0300

    router ekelntileri ekleniyo

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/template/html"
 	"github.com/omerfruk/game-box/database"
 	"github.com/omerfruk/game-box/routers"
-	"log"
 )
 
 func main() {
 	engine := html.New("views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
 	app.Use(cors.New())
-
+	// deneme islemleri 2
 	database.ConnectAndMigrate()
 	routers.Router(app)
 
