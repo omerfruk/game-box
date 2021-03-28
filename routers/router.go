4b7872187076f4edb4d5cbd8f5f96fa879a3c1a3
omerfruk <omer.fruk3547@gmail.com>
2021-03-28 16:18:16 +0300

    router guruplandırıldı

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/omerfruk/game-box/handlers"
 )
@@ -17,8 +18,24 @@ func Router(app *fiber.App) {
 	app.Get("/login", handlers.LoginGet)
 	app.Post("/login", handlers.LoginPost)
 	app.Get("/logout", handlers.Logout)
-	//deneme yönlendiricisi
+	//developers yönlendiricisi
 	app.Get("/developers", handlers.Developers)
+
+	//session kontrol
+	add := app.Group("/account", sessionControl)
 	//dashboard yonlendiricisi
-	app.Get("/dashboard", handlers.DashboardGet)
+	add.Get("/dashboard", handlers.DashboardGet)
+	//developer yönlendirici
+	add.Get("/developer", handlers.Developers)
+}
+func sessionControl(c *fiber.Ctx) error {
+	sess, err := handlers.Store.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if sess.Get("name") == nil {
+		return c.Redirect("/login")
+	}
+	return c.Next()
+
 }
