e9f9193825e8c96aed408130cd899c39d657566e
omerfruk <omer.fruk3547@gmail.com>
2021-03-28 15:25:18 +0300

    navbar eklentisi icin struct donduruldu

--- handlers/developers.go
+++ handlers/developers.go
@@ -11,9 +11,17 @@ import (
 
 func Developers(c *fiber.Ctx) error {
 	var temp []models.Developer
-	err := database.DB().Find(&temp).Error
+	sess, err := Store.Get(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = database.DB().Find(&temp).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		fmt.Println(err)
 	}
-	return c.Render("developers", temp)
+	d := models.Islogin{
+		Developer: temp,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("developers", d)
 }
