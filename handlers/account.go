8de67aca6330d4561c6cc802bb591e816d0519de
omerfruk <omer.fruk3547@gmail.com>
2021-04-09 00:29:33 +0300

    account sayfası için handler yazildi

---
+++ handlers/account.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/game-box/service"
+	"strconv"
+)
+
+func GetAccount(c *fiber.Ctx) error {
+	id, _ := strconv.ParseUint(c.Params("key"), 10, 32)
+	account := service.GetDeveloperById(uint(id))
+
+	return c.Render("account", account)
+}
