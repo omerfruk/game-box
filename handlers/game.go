1c9ab92edfda88c6ce622ff90f475c7b3d258af1
omerfruk <omer.fruk3547@gmail.com>
2021-06-01 21:01:37 +0300

    biseyler yapildi

--- handlers/game.go
+++ handlers/game.go
@@ -1,21 +1,53 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/game-box/models"
+	"github.com/omerfruk/game-box/service"
+)
 
 func SnakeGame(c *fiber.Ctx) error {
-	return c.Render("snake", fiber.Map{
-		"Bool": true,
-	})
+	tempDevelopers := service.GetDeveloperAll()
+	sess := service.GetSesion(c)
+	a := models.Islogins{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("snake", a)
 }
 func Sayitoplama(c *fiber.Ctx) error {
-	return c.Render("sayi_topla", true)
+	tempDevelopers := service.GetDeveloperAll()
+	sess := service.GetSesion(c)
+	a := models.Islogins{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("sayi_topla", a)
 }
 func Mario(c *fiber.Ctx) error {
-	return c.Render("mario", true)
+	tempDevelopers := service.GetDeveloperAll()
+	sess := service.GetSesion(c)
+	a := models.Islogins{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("mario", a)
 }
 func BlackJack(c *fiber.Ctx) error {
-	return c.Render("blackjack", true)
+	tempDevelopers := service.GetDeveloperAll()
+	sess := service.GetSesion(c)
+	a := models.Islogins{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("blackjack", a)
 }
 func Dino(c *fiber.Ctx) error {
-	return c.Render("dino", true)
+	tempDevelopers := service.GetDeveloperAll()
+	sess := service.GetSesion(c)
+	a := models.Islogins{
+		Developer: tempDevelopers,
+		Bool:      !sess.Fresh(),
+	}
+	return c.Render("dino", a)
 }
